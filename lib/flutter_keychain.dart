f52c0c85ad99e015b52655d787093c9b4e0117c7
wbusey0 <wietse.buseyne@cumulus.uzleuven.be>
2021-04-12 15:25:37 +0200

    Migrate to dart 2.12 + added null-safety (NNBD)

--- lib/flutter_keychain.dart
+++ lib/flutter_keychain.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:meta/meta.dart';
 
 class FlutterKeychain {
   static const MethodChannel _channel =
@@ -9,15 +8,15 @@ class FlutterKeychain {
 
   // put - store the value for a key.
   static Future<void> put(
-          {@required String key, @required String value}) async =>
+          {required String key, required String value}) async =>
       _channel.invokeMethod('put', {'key': key, 'value': value});
 
   // get - get the value for a given key
-  static Future<String> get({@required String key}) async =>
+  static Future<String?> get({required String key}) async =>
       await _channel.invokeMethod('get', {'key': key});
 
   // remove - remove entry for a given key
-  static Future<void> remove({@required String key}) async =>
+  static Future<void> remove({required String key}) async =>
       await _channel.invokeMethod('remove', {'key': key});
 
   // clear - clear the keychain
