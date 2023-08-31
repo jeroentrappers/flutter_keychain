4300150bd1682a6d29be59c3130e4c75e67ec1b4
Jeroen Trappers <jeroen.trappers@gmail.com>
2023-08-31 16:18:06 +0200

    Bump example gradle version

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/jeroentrappers/fvm/versions/2.10.0"
+export "FLUTTER_ROOT=/Users/jeroentrappers/fvm/versions/3.13.1"
 export "FLUTTER_APPLICATION_PATH=/Users/jeroentrappers/Developer/github/flutter_keychain/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/jeroentrappers/Developer/github/flutter_keychain/example/lib/main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NAME=1.0.1"
 export "FLUTTER_BUILD_NUMBER=1"
-export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/jeroentrappers/Developer/github/flutter_keychain/example/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
